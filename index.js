052b084569067e4c71835b848d879ec9f97cad5b
Demo User <demouser@MacBook-Air-4.local>
2016-07-14 09:03:35 -0700

    starting to set up some jQuery

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// We are the spaghetti kangaroos!!
+function setup() {
+  /*$('#summary').on('click',function() {
+    $('#openSum').hide();
+  });
+  $('#hometown').on('click',function() {
+    $('#openHome').show();
+  });
+  $('#university').on('click',function() {
+    $('#openUni').show();
+  });
+  $('#social').on('click',function() {
+    $('#openSoc').show();
+  });*/
+  $('#button').on('click', function() {
+    $('openHome').toggleClass('hideHome');
+  });
+  /*$(function() {
+    $()
+  });*/
+}
+
+$(document).ready(setup);
